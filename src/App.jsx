ca2c468985088eb110540085b7a848f074d8bb44
Salman-sajib <sfsajib@gmail.com>
2024-12-25 15:31:04 +0600

    changed the background from dark to light

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,15 @@ import BackgroundDots from "./components/BackgroundDots";
 function App() {
   return (
     <div>
-      <div className="relative bg-zinc-950 min-h-screen flex items-center justify-center">
+      <div className="relative bg-neutral-100 min-h-screen flex items-center justify-center">
         <BackgroundDots
-          dotColor="#334155"
-          dotSize={1}
+          dotColor="#e5e5e5"
+          dotSize={1.5}
           spacing={20}
           className="absolute inset-0 z-0"
         />
 
-        <div className="relative z-10 text-center mt-10 text-neutral-200">
+        <div className="relative z-10 text-center mt-10 text-gray-600">
           <h1 className="text-6xl font-bold">Welcome to My App</h1>
           <p className="text-xl mt-4">
             Enjoy the perfectly aligned dotted background!
