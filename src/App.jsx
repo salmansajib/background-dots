bba9c928b1c54398646d81bef0adb9606b6ea2ad
Salman-sajib <sfsajib@gmail.com>
2024-12-25 15:40:19 +0600

    changed some tags and styles

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,24 @@ import BackgroundDots from "./components/BackgroundDots";
 function App() {
   return (
     <div>
-      <div className="relative bg-neutral-100 min-h-screen flex items-center justify-center">
-        <BackgroundDots
-          dotColor="#e5e5e5"
-          dotSize={1.5}
-          spacing={20}
-          className="absolute inset-0 z-0"
-        />
-
-        <div className="relative z-10 text-center mt-10 text-gray-600">
-          <h1 className="text-6xl font-bold">Welcome to My App</h1>
-          <p className="text-xl mt-4">
-            Enjoy the perfectly aligned dotted background!
-          </p>
+      <main>
+        <div className="relative bg-neutral-100 min-h-screen flex items-center justify-center">
+          <BackgroundDots
+            dotColor="#e5e5e5"
+            dotSize={1.5}
+            spacing={20}
+            className="absolute inset-0 z-0"
+          />
+          <div className="relative z-10 text-center mt-10 p-4">
+            <h1 className="text-6xl font-bold text-gray-900">
+              Welcome to My App
+            </h1>
+            <p className="text-lg mt-4 text-gray-500">
+              Enjoy the perfectly aligned dotted background!
+            </p>
+          </div>
         </div>
-      </div>
+      </main>
     </div>
   );
 }
